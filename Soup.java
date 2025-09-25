52306b41a4e57a1f056886b63ae1e17571e91e41
coolight543 <robamiao@gmail.com>
2025-09-25 16:44:26 +0000

    done2

--- Soup.java
+++ Soup.java
@@ -29,35 +29,49 @@ public class Soup {
 
     //adds a word to the pool of letters known as "letters"
     public void add(String word){
-
+        letters += word;
     }
 
 
     //Use Math.random() to get a random character from the letters string and return it.
     public char randomLetter(){
-        return 'a';
+        int randomCharacter = (int)(Math.random()* (letters.length()));
+        return letters.charAt(randomCharacter);
     }
 
 
     //returns the letters currently stored with the company name placed directly in the center of all
     //the letters
     public String companyCentered(){
-        return "";
+        String companyCentered  =  letters.substring(0,(letters.length()+1)/2);
+        companyCentered += company;
+        companyCentered += letters.substring((letters.length()+1)/2); 
+        return companyCentered;
     }
+
 
 
     //should remove the first available vowel from letters. If there are no vowels this method has no effect.
     public void removeFirstVowel(){
-        
+        String noAs = letters.replace("a","");
+        String noEs = letters.replace("e", "");
+        String noIs = letters.replace("i", "");
+        String noOs = letters.replace("o", "");
+        String noUs = letters.replace("u",""); 
     }
 
     //should remove "num" letters from a random spot in the string letters. You may assume num never exceeds the length of the string.
     public void removeSome(int num){
+        int someNum = letters.substring(0, Math.random()); 
+        String letters -= someNum;
+        System.out.print(num);
 
     }
 
     //should remove the word "word" from the string letters. If the word is not found in letters then it does nothing.
     public void removeWord(String word){
+    letters -= word; 
+    return letters; 
         
     }
 }
