1afd0f2761d5d6c94fa39c1fd358134ed7f0321e
coolight543 <robamiao@gmail.com>
2025-09-26 13:45:36 +0000

    done926

--- Soup.java
+++ Soup.java
@@ -53,18 +53,20 @@ public class Soup {
 
     //should remove the first available vowel from letters. If there are no vowels this method has no effect.
     public void removeFirstVowel(){
-        String noAs = letters.replace("a","");
-        String noEs = letters.replace("e", "");
-        String noIs = letters.replace("i", "");
-        String noOs = letters.replace("o", "");
-        String noUs = letters.replace("u",""); 
+        String noAs = letters.replace('a',"");
+        String noEs = letters.replace('e', "");
+        String noIs = letters.replace('i', "");
+        String noOs = letters.replace('o', "");
+        String noUs = letters.replace('u',""); 
     }
 
     //should remove "num" letters from a random spot in the string letters. You may assume num never exceeds the length of the string.
     public void removeSome(int num){
-        int someNum = letters.substring(0, Math.random()); 
-        String letters -= someNum;
-        System.out.print(num);
+
+        int start = (int)(Math.random()*letters.length())-num;
+        String removeSomeLetters = letters.substring(0, start)+ letters.substring(start+num);
+        String letters -= removeSomeLetters;
+        System.out.print(letters);
 
     }
 
