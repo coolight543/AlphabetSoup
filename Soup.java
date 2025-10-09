5a5eaafb8c8a214697ebfec59ad9f71743aac77f
coolight543 <robamiao@gmail.com>
2025-10-09 18:18:37 +0000

    save 10 9 2025

--- Soup.java
+++ Soup.java
@@ -1,3 +1,6 @@
+// Kuang Miao
+// This code will choose random/assigned letters or words to remove if there's letters added to string of letters, and center the company name when instructed
+
 public class Soup {
     //these are instance variables 
     private String letters;
@@ -28,24 +31,27 @@ public class Soup {
 //below are the functions you'll be writing.
 
     //adds a word to the pool of letters known as "letters"
-    public void add(String word){
+    //precondition: letters are english characters, must add the word "add " to get letters
+    public void add(String word){ 
         // letters = letters + word: contactenation
-        letters += word;
+        letters += word;}
         // public void no return
-    }
+        // postcondition：the string cotains "bew letters is" + the word I added
 
 
     //Use Math.random() to get a random character from the letters string and return it.
+    // precondition: Input must be the letters - randomLetter, it will not work if you type anything else
     public char randomLetter(){
         // make a new int, find a random place on the whole string of letters
         int randomCharacter = (int)(Math.random()* (letters.length()));
         //return the letter, a single letter, can be represented as char
         return letters.charAt(randomCharacter);
+        // postcondition: the char returns a random character from the letters that typed previously in the add section
     }
 
 
     //returns the letters currently stored with the company name placed directly in the center of all
-    //the letters
+    //precondition: String company is not null, and called the method using "centered" and press enter
     public String companyCentered(){
         //create a string, choose from beginning to the midpoint of the whole string of letters, using midpoint formula
         String companyCentered  =  letters.substring(0,(letters.length()+1)/2);
@@ -55,19 +61,23 @@ public class Soup {
         companyCentered += letters.substring((letters.length()+1)/2); 
         //public string return
         return companyCentered;
+        //postcondition: company name is print in the middle of the string of letters typed previously
     }
 
 
 
     //should remove the first available vowel from letters. If there are no vowels this method has no effect.
+    // Precondition: the string of letters is not null, call the command by calling "removeVowel"
     public void removeFirstVowel(){
         //Find the first vowels to replace, which are searching AEIOU && aeiou, replace with an empty string
         letters = letters.replaceFirst("[AEIOUaeiou]","");
         //public void no return, use System.out.print instead
         System.out.print(letters);
+        //postcondition: The first vowel found in the string of letters will be removed
     }
 
     //should remove "num" letters from a random spot in the string letters. You may assume num never exceeds the length of the string.
+    // precondition: num must be an integer > 0, string of letters is not null, and num < length of string, type removeSome + an integer (ex: removeSome 5) to use command
     public void removeSome(int num){
         //create an int start to find where the num start to count
         int start = (int)(Math.random()*(letters.length()-num));
@@ -75,10 +85,11 @@ public class Soup {
         letters  = letters.substring(0, start)+ letters.substring(start+num);
         //public void no return
         System.out.print(letters);
-
+        //postcondition: string contains the letters kept
     }
 
     //should remove the word "word" from the string letters. If the word is not found in letters then it does nothing.
+    //precondition: String word and letters is not null, use removeWord +  word (removeWord koko) to command
     public void removeWord(String word){
         //remove the index of the word, create the integer for what to return
         int num = letters.indexOf(word); 
@@ -86,5 +97,7 @@ public class Soup {
         letters = letters.replace(letters.substring(num, num+word.length()),"");
         //public void no return
         System.out.print(letters);
+        //postcondition: remove the word in the string from string letters if found
         }
+    
 }
